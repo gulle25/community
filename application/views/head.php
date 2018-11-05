e14195feb9b67d60ac2805df0c56898204f3e70c
gulle25 <gulle25@hanmail.net>
2018-11-06 08:59:49 +0900

    remove appstack

--- application/views/head.php
+++ application/views/head.php
@@ -1,22 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
-<!-- <html>
-  <head>
-    <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/application/libraries/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
-    <link href="/application/libraries/bootstrap/css/bootstrap-grid.min.css" rel="stylesheet">
-    <link href="/application/libraries/bootstrap/css/bootstrap-reboot.min.css" rel="stylesheet">
-    <link href="/application/libraries/etc/css/simple-sidebar.css" rel="stylesheet" media="screen">
-    <link rel="stylesheet" type="text/css" href="/application/libraries/semantic/semantic.min.css">
-    <script src="http://code.jquery.com/jquery.js"></script>
-    <script src="/application/libraries/semantic/semantic.min.js"></script>
-  </head>
-  <body>
- -->
-
-
 <html lang="kr">
 <head>
   <meta charset="utf-8">
